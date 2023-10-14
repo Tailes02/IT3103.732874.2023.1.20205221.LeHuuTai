6d4f42b637a8a8077ccd881b8e9d422e5e861bb9
Tailes02 <taihuule02@gmail.com>
2023-10-14 16:05:33 +0700

    bai1

---
+++ lab01/cal.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class cal {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the first number: ");
+        String strNum1 = scanner.nextLine();
+
+        System.out.print("Enter the second number: ");
+        String strNum2 = scanner.nextLine();
+
+        double num1 = Double.parseDouble(strNum1);
+        double num2 = Double.parseDouble(strNum2);
+
+        double sum = num1 + num2;
+        double difference = num1 - num2;
+        double product = num1 * num2;
+
+        if (num2 != 0) {
+            double quotient = num1 / num2;
+            System.out.println("Sum: " + sum);
+            System.out.println("Difference: " + difference);
+            System.out.println("Product: " + product);
+            System.out.println("Quotient: " + quotient);
+        } else {
+            System.out.println("Error: Division by zero is not allowed.");
+        }
+
+        scanner.close();
+    }
+}
